cabdbf5217f5decf0e362acb7809b5bdcb269c06
rdesfonds-campus <romain.desfonds@gmail.com>
2026-04-24 16:43:37 +0200

    exercice 7

--- 7-async-await/script.js
+++ 7-async-await/script.js
@@ -5,7 +5,10 @@ async function main() {
   // il n'est pas possible d'utiliser await en dehors d'une fonction async
   // vous devez donc appeler les fonctions que vous allez écrire ci dessous
 
-  // wait 100 ms
+ fetch('https://swapi.dev/api/species/1/')
+  .then(response => response.json())
+  .then(data => console.log(data));
+  
   console.log("- 2 - BEFORE await");
   await new Promise( resolve => setTimeout(resolve, 10));
   console.log("- 3 - AFTER await");
