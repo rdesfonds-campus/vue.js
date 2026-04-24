69f0618c16b08853795ff547b5172f88877c1f00
rdesfonds-campus <romain.desfonds@gmail.com>
2026-04-24 14:30:43 +0200

    exercice 4

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -3,10 +3,12 @@
 const [a, b] = [1, 2, 3, 4];
 console.log(a);
 console.log(b);
+//affichera a et b et ignorera les autres éléments du tableau
 
-const { first, last } = { first: 'Paul', last: 'Henta', age: 35 };
+const { first, last} = { first: 'Paul', last: 'Henta', age: 35 };
 console.log(first);
 console.log(last);
+//le console log affichera les clés first et last et ignorera age
 
 
 // 4.2 Application
@@ -14,7 +16,40 @@ console.log(last);
 // Transformer l'objet data en tableau de perturbations
 const perturbations = Object.values(data);
 
+
 // Pour chacune, récupérer texte, dateDebut, dateFin
 perturbations.forEach(({ texte, dateDebut, dateFin }) => {
   console.log(texte, dateDebut, dateFin);
 });
+
+// 4.3 – Destructuring des paramètres d'une fonction
+function addPerturbation({
+  id,
+  texte,
+  dateDebut,
+  dateFin,
+  type = 'info',
+  heureDebut = '00:00:00',
+  heureFin = '23:59:59',
+  latitude = -1,
+  longitude = -1
+}) {
+  data[id] = {
+    id,
+    texte,
+    dateDebut,
+    dateFin,
+    type,
+    heureDebut,
+    heureFin,
+    latitude,
+    longitude
+  };
+}
+
+// 4.4 – Fonction avec opérateur rest (...)
+function logAll(...args) {
+  console.log(args);
+}
+
+logAll(1, 'a', true);
