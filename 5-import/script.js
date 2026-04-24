e96a5c13f277b5c8cc1a63b53f9cf42cccc39c82
rdesfonds-campus <romain.desfonds@gmail.com>
2026-04-24 15:24:36 +0200

    exercice 5

--- 5-import/script.js
+++ 5-import/script.js
@@ -1,8 +1,23 @@
-//5-1 Import
+// 5-1 Import
 
-import {hello} from './functions.js';
+import helloNyan, { hello, showError } from './functions.js';
+import { sum, sub, multiply, divide, pow } from './math.js';
 
+// On utilise hello
 hello();
 
+// On utilise showError
+showError('Une erreur est survenue');
+
+// On utilise la fonction anonyme par défaut (ici renommée helloNyan)
+helloNyan();
+
 // 5-2 Export
+
 const [a, b] = [7, 9];
+
+console.log(sum(a, b));
+console.log(sub(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
+console.log(pow(a, b));
