b3b416e90a661a51fa67a8bc5e0865af82bf5bac
rdesfonds-campus <romain.desfonds@le-campus-numerique.fr>
2026-04-27 01:26:19 +0200

    exo 6/ 7

--- 6-promises/script.js
+++ 6-promises/script.js
@@ -1,28 +1,102 @@
-// Fonction utilisées par les fonctions de l'exercice - Ne pas modifier ou appeler directement
+/*Section 1 */
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Note : les fonctions ci-dessous sont prêtes à l'emploi, vous n'avez pas à les modifier, juste à les appeler.
-// Elles retournent toutes des promesses et peuvent donc être chaînées via la fonction "then".
 function first() {
-  return sleep(300).then(() => {console.log('message 1');});
+  return sleep(300).then(() => { console.log('message 1'); });
 }
 
 function second() {
-  return sleep(100).then(() => {console.log('message 2');});
+  return sleep(100).then(() => { console.log('message 2'); });
 }
 
 function third() {
-  return sleep(200).then(() => {console.log('message 3');});
+  return sleep(200).then(() => { console.log('message 3'); });
 }
 
 function secondWithError() {
-  return sleep(100).then(() => {throw new Error("catch me if you can");})
+  return sleep(100).then(() => { throw new Error("catch me if you can"); });
 }
 
 first()
-.then(() => secondWithError())
+  .then(() => second())
+  .then(() => third());
+
+first()
+  .then(() => secondWithError())
   .catch((error) => {
     console.error(error.message);
-  })  .then(() => third());
+  })
+  .then(() => third());
+
+
+/* Section 2
+
+function getHumanSpecies() {
+  return fetch('https://swapi.dev/api/species/1/')
+    .then(response => response.json());
+}
+
+function getFirstHumanCharacter() {
+  return getHumanSpecies()
+    .then(species => fetch(species.people[0]))
+    .then(response => response.json());
+}
+
+function getAllPeopleNamesFromSpecies() {
+  return getHumanSpecies()
+    .then(species => {
+      const peoplePromises = species.people.map(url => {
+        return fetch(url).then(response => response.json());
+      });
+
+      return Promise.all(peoplePromises);
+    })
+    .then(people => {
+      people.forEach(person => {
+        console.log(person.name);
+      });
+    });
+}
+
+getHumanSpecies().then(species => {
+  console.log("Espèce :", species.name);
+});
+
+getFirstHumanCharacter().then(character => {
+  console.log("Premier personnage :", character.name);
+});
+
+getAllPeopleNamesFromSpecies();
+
+*/
+
+
+/* scetion 3
+
+function waitForUserChoice() {
+  return new Promise((resolve, reject) => {
+    const okBtn = document.getElementById('okBtn');
+    const cancelBtn = document.getElementById('cancelBtn');
+
+    okBtn.addEventListener('click', () => {
+      resolve('Ok clicked');
+    }, { once: true });
+
+    cancelBtn.addEventListener('click', () => {
+      reject('Cancel clicked');
+    }, { once: true });
+  });
+}
+
+waitForUserChoice()
+  .then(message => {
+    console.log(message);
+  })
+  .catch(message => {
+    console.log(message);
+  });
+
+*/
--- 7-async-await/script.js
+++ 7-async-await/script.js
@@ -1,16 +1,16 @@
-
 async function main() {
   console.log("- 1 - START of main function");
 
-  // il n'est pas possible d'utiliser await en dehors d'une fonction async
-  // vous devez donc appeler les fonctions que vous allez écrire ci dessous
+  try {
+    const response = await fetch('https://swapi.dev/api/species/1/');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 
- fetch('https://swapi.dev/api/species/1/')
-  .then(response => response.json())
-  .then(data => console.log(data));
-  
   console.log("- 2 - BEFORE await");
-  await new Promise( resolve => setTimeout(resolve, 10));
+  await new Promise(resolve => setTimeout(resolve, 10));
   console.log("- 3 - AFTER await");
 
   console.log("- 4 - END of main function");
