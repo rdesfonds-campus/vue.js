9f039829a549488c6ee3001759712c528af2548d
rdesfonds-campus <romain.desfonds@le-campus-numerique.fr>
2026-04-30 21:10:51 +0200

    3 ajout CitiesList.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,52 +1,24 @@
 <template>
-  <div id="app">
-    <h1 :class="{ red: isRed }">{{ title }}</h1>
-    <h2 :class="{ red: isRed }">Sous-titre</h2>
+  <main>
+    <h1>Météo</h1>
+    <button @click="onListClick">Voir la liste des villes</button>
 
-    <input v-model="draftTitle" type="text" />
-    <button @click="updateTitle">Mettre à jour le titre</button>
-
-    <label>
-      <input v-model="isRed" type="checkbox" />
-      Mettre les titres en rouge
-    </label>
-
-    <br />
-
-    <label>
-      <input v-model="showImage" type="checkbox" />
-      Afficher l'image
-    </label>
-
-    <img
-      v-if="showImage"
-      src="https://vuejs.org/images/logo.png"
-      alt="Logo Vue"
-      width="120"
-    />
-  </div>
+    <CitiesList />
+  </main>
 </template>
 
 <script>
+import CitiesList from './views/CitiesList.vue'
+
 export default {
-  data() {
-    return {
-      title: 'Bonjour Vue',
-      draftTitle: '',
-      isRed: false,
-      showImage: true
-    }
+  name: 'App',
+  components: {
+    CitiesList
   },
   methods: {
-    updateTitle() {
-      this.title = this.draftTitle
+    onListClick() {
+      alert('Tu as cliqué sur la liste des villes')
     }
   }
 }
 </script>
-
-<style>
-.red {
-  color: red;
-}
-</style>
---
+++ vue-project/src/views/CitiesList.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Météo - Liste des villes</h1>
+</template>
+
+<script>
+export default {
+  name: 'CitiesList'
+}
+</script>
